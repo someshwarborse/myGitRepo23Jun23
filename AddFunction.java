d8558e772d9cdc604b89a96a471ee1cb9d76c8d2
Someshwar <someshwarborse12@ymail.com>
2023-07-05 12:26:50 +0530

    my task completed

--- AddFunction.java
+++ AddFunction.java
@@ -1,2 +1,6 @@
 THIS IS ADD FUNCTION
-my manager work completed
+1st day java lines of code
+2nd day java lines of code
+3rd day java lines of code
+4th day java lines of code
+5th day java lines of code
